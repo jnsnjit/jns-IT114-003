2fc2878d77867b98ce1e93d8b9d19c4b020a16b0
jnsnjit <156932213+jnsnjit@users.noreply.github.com>
2024-09-11 16:15:23 -0400

    test java file, connection between vscode and github repo is all good now

---
+++ hi.java
@@ -0,0 +1,14 @@
+class hi{
+    public static void main(String[] args){
+        System.out.println("hello world");
+        System.out.println(recursion(5));
+    }
+    public static int recursion(int x){
+        if(x==0){
+            return 1;
+        }
+        return x * recursion(x-1);
+    }
+}
+//comment, class hi, just a test
+
